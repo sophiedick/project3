fb134671d872793dffa75381576a4a12fed001f9
CarolineBenn <lineybenn@gmail.com>
2016-01-31 22:37:22 +0000

    Added updateThread function and routes

--- config/routes.js
+++ config/routes.js
@@ -18,6 +18,13 @@ router.route('/api/category')
   .post(threadsController.createThread)
 
 
+router.route('/category/:id')
+  .get(threadsController.showThread)
+  .put(threadsController.updateThread);
+
+router.route('/category/:id/edit')
+  .get(threadsController.editThread)
+
 //router.route('/signup')
 //  .get(unAuthenticatedUser, usersController.getSignup)
 //  .post(usersController.postSignup)
--- controllers/threadsController.js
+++ controllers/threadsController.js
@@ -5,12 +5,13 @@ var Thread = require("../models/thread");
 
 function home(req, res) {  
   res.render('index.ejs'); // { message: req.flash('errorMessage') });
-}
+};
 
 function category(req, res) {
   res.render('category.ejs');
-}
+};
 
+/* POST NEW THREAD */
 function createThread(req, res) {
   var thread = new Thread(req.body.thread);
   thread.save(function(err, thread){
@@ -18,30 +19,68 @@ function createThread(req, res) {
     res.status(201).send(thread);
     res.render('category.ejs')
   });
-}
+};
 
+/* GET THREAD INDEX */
 function threadIndex(req, res){
   Thread.find(function(err, threads){
-    if (err) return res,status(404).json({message: 'Something went wrong'});
+    if (err) return res.status(404).json({ message: 'Something went wrong'});
     res.status(200).json({ threads: threads });
     res.render('category.ejs');
-  })
-}
+  });
+};
 
-//HAVE YOU EXPORTED?????
-//HAVE YOU EXPORTED?????
-//HAVE YOU EXPORTED?????
-//HAVE YOU EXPORTED?????
-//HAVE YOU EXPORTED?????
-//HAVE YOU EXPORTED?????
-//HAVE YOU EXPORTED?????
-//HAVE YOU EXPORTED?????
-//HAVE YOU EXPORTED?????
+
+/* SHOW SINGLE THREAD */
+function showThread(req, res){
+  var id = req.params.id;
+  Thread.findById({_id: id}, function(err, thread){
+    if(err) res.json({message: 'Could not find thread because:' + err});
+    var thread2 = thread
+    res.render('single',{ thread: thread})
+  });
+};
+
+/* But where's the editThread function? */
+// Edit thread form will be implemented with jQuery and Ajax in place - CB
+
+
+/* UPDATE THREAD */
+function updateThread(req, res){
+  var id = req.params.id;
+
+  Thread.findById({_id: id}, function(err, thread){
+    if (err) return res.status(404).json({ message: 'Something went wrong trying to update thread.'});
+    if (req.body.topic) thread.topic = req.body.topic;
+    if (req.body.title) thread.title = req.body.title;
+    if (req.body.body)  thread.body  = req.body.body;
+    thread.modifiedAt = Date.now;   // This updates the modifiedAt column to current time
+
+    thread.save(function(err){
+      if (err) res.status(400).json({ message: 'Could not update thread because: ' + err });
+      res.json({ message: 'Thread successfully updated.' });
+    });
+  });
+};
+
+
+//HAVE YOU EXPORTED????? //HAVE YOU EXPORTED????? 
+//HAVE YOU EXPORTED????? //HAVE YOU EXPORTED????? 
+//HAVE YOU EXPORTED????? //HAVE YOU EXPORTED????? 
+//HAVE YOU EXPORTED????? //HAVE YOU EXPORTED????? 
+//HAVE YOU EXPORTED????? //HAVE YOU EXPORTED????? 
+//HAVE YOU EXPORTED????? //HAVE YOU EXPORTED????? 
+//HAVE YOU EXPORTED????? //HAVE YOU EXPORTED????? 
+//HAVE YOU EXPORTED????? //HAVE YOU EXPORTED????? 
+//HAVE YOU EXPORTED????? //HAVE YOU EXPORTED????? 
+
 
 module.exports = {
   home:         home,
   category:     category,
   createThread: createThread,
-  threadIndex:  threadIndex
+  threadIndex:  threadIndex,
+  updateThread: updateThread,
+  showThread:   showThread
 }
 
