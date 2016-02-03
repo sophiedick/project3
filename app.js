cde79b5dac861e7a924577e98999e23c78722d96
sophiedick <sophiefdick@gmail.com>
2016-02-03 19:12:30 +0000

    Post permissions sorted, cant make comment unless logged in

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(passport.initialize());
 /////////////// protect route////////////////////////////
 // unless logged in(ie unless you have a token) then you cannot make a post
 app.post('/api/category', expressJWT({ secret: secret }));
+app.post('/api/category/:id/newcomment', expressJWT({ secret: secret }));
 // app.put('/api/someotherurl', expressJWT({ secret: secret }));
 
 // route error handler
